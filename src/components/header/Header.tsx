8c0114fab9b8b373a9aef1a11b76cc3a1eb9871f
Kyle Close <k.james.close@gmail.com>
2023-10-12 19:08:13 -0700

    refactor tw class names

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,11 +7,11 @@ import menuImg from "../../assets/menu-icon.png";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center border-b border-white h-16 bg-header shadow-md px-4 relative">
-      <div className="w-8">
+    <div className={tw_headerContainer}>
+      <div className={tw_imgContainer}>
         <Menu menuImg={menuImg} />
       </div>
-      <div className="flex gap-4 text-sm">
+      <div className={tw_rightSide}>
         <HeaderButton url="/signup" buttonColor="green" text="Sign up" />
         <HeaderButton url="/signin" buttonColor="blue" text="Log in" />
       </div>
@@ -21,3 +21,20 @@ function Header() {
 }
 
 export default Header;
+
+const tw_headerContainer = [
+  "flex",
+  "justify-between",
+  "items-center",
+  "border-b",
+  "border-white",
+  "h-16",
+  "bg-header",
+  "shadow-md",
+  "px-4",
+  "relative",
+].join(" ");
+
+const tw_imgContainer = ["w-8"].join(" ");
+
+const tw_rightSide = ["flex", "gap-2", "text-sm"].join(" ");
