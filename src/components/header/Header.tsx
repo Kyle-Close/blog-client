0dd1b360f36f8542e3862a90c3303bee22186c58
Kyle Close <k.james.close@gmail.com>
2023-10-12 20:20:21 -0700

    refactored header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,8 +8,15 @@ import menuImg from "../../assets/menu-icon.png";
 function Header() {
   return (
     <div className={tw_headerContainer}>
-      <div className={tw_imgContainer}>
-        <Menu menuImg={menuImg} />
+      <div className={tw_leftSide}>
+        <div className={tw_imgContainer}>
+          <Menu menuImg={menuImg} />
+        </div>
+        <div className={tw_linksLeftSide}>
+          <a href="/">Web Dev</a>
+          <a href="/">Electronics</a>
+          <a href="/">Gaming</a>
+        </div>
       </div>
       <div className={tw_rightSide}>
         <HeaderButton url="/signup" buttonColor="green" text="Sign up" />
@@ -29,12 +36,26 @@ const tw_headerContainer = [
   "border-b",
   "border-white",
   "h-16",
+  "sm:h-24",
   "bg-header",
   "shadow-md",
   "px-4",
+  "sm:px-10",
   "relative",
 ].join(" ");
 
-const tw_imgContainer = ["w-8"].join(" ");
+const tw_imgContainer = ["sm:hidden", "w-8"].join(" ");
 
-const tw_rightSide = ["flex", "gap-2", "text-sm"].join(" ");
+const tw_rightSide = ["flex", "gap-2", "sm:gap-4", "text-sm"].join(" ");
+
+const tw_linksLeftSide = [
+  "hidden",
+  "grow",
+  "font-semibold",
+  "sm:flex",
+  "sm:gap-6",
+  "md:gap-10",
+  "md:text-lg",
+].join(" ");
+
+const tw_leftSide = ["flex", "grow"].join(" ");
