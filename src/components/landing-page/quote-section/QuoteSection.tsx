827656b662eaa92121f5ce57646fc8f11ad022c2
Kyle Close <k.james.close@gmail.com>
2023-10-24 12:18:23 -0700

    more quote section progress

--- src/components/landing-page/quote-section/QuoteSection.tsx
+++ src/components/landing-page/quote-section/QuoteSection.tsx
@@ -1,6 +1,7 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import portait1 from "../../../assets/portrait1.jpg";
+import quoteIcon from "../../../assets/quoteIcon.svg";
 
 function QuoteSection() {
   return (
@@ -12,11 +13,24 @@ function QuoteSection() {
           showThumbs={false}
           showStatus={false}
         >
-          <div className="flex flex-col items-center py-8">
+          <div className="flex flex-col items-center py-8 gap-8">
             <div className="basis-2/5 max-w-[40%] border-2 rounded-md shadow-sm shadow-white">
-              <img className="" src={portait1} />
+              <img src={portait1} />
             </div>
-            <div className="flex basis-1/2 grow bg-red-200"></div>
+            <div className="p-4 mb-4 text-start flex flex-col items-start gap-4 bg-slate-900 w-64 rounded-lg text-sm">
+              <div className="w-10">
+                <img src={quoteIcon} />
+              </div>
+              <p>
+                Our people hold the keys to their own future. Pando helps
+                empower people to understand where they’re at and how to move
+                forward in their career.
+              </p>
+              <div className="flex flex-col items-start text-xs">
+                <p className="font-medium">Monica Matison</p>
+                <p>Vice President of People, Shipwell</p>
+              </div>
+            </div>
           </div>
           <div>
             <img src={portait1} />
