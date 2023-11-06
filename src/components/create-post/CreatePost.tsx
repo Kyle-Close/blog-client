d543ef00fc499295d9bfd7a923348449b3fc817d
Kyle-Close <k.james.close@gmail.com>
2023-11-05 17:44:41 -0700

    small refactor

--- src/components/create-post/CreatePost.tsx
+++ src/components/create-post/CreatePost.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import { useModal } from '../../hooks/useModal';
+import CreatePostModal from './CreatePostModal';
 
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
 
 import axios from 'axios';
 
 function CreatePost() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const { open, handleOpen, handleClose } = useModal();
 
   const editorRef = useRef<any>(null);
   const [title, setTitle] = React.useState<string | null>(null);
@@ -23,18 +21,6 @@ function CreatePost() {
     color: 'white',
     fontWeight: '600',
     backgroundColor: '#2e2e2e',
-  };
-
-  const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
   };
 
   const submitPost = async (e: any) => {
@@ -63,21 +49,7 @@ function CreatePost() {
 
   return (
     <Box component='form' className={tw_wrapper}>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
-        <Box sx={modalStyle}>
-          <Typography id='modal-modal-title' variant='h6' component='h2'>
-            Text in a modal
-          </Typography>
-          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+      <CreatePostModal open={open} handleClose={handleClose} />
       <div className={tw_container}>
         <div className={tw_top}>
           <div className={tw_titleAndButton}>
---
+++ src/components/create-post/CreatePostModal.tsx
@@ -0,0 +1,42 @@
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface CreatePostModalProps {
+  open: boolean;
+  handleClose: () => void;
+}
+
+function CreatePostModal({ open, handleClose }: CreatePostModalProps) {
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+    >
+      <Box sx={modalStyle}>
+        <Typography id='modal-modal-title' variant='h6' component='h2'>
+          Text in a modal
+        </Typography>
+        <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
+  );
+}
+
+export default CreatePostModal;
